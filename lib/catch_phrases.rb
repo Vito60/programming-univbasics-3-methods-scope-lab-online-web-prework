26e407918fbe5e03160e595a81cc600318ddd65c
Randall D Williams <randallw1996@gmail.com>
2019-11-11 02:46:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
 def mario
   $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  $phrase = "It's-a me, Mario!"
-  puts $phrase
+  phrase = "It's-a me, Mario!"
+  puts phrase
 end
 
 def toadstool
@@ -9,13 +9,12 @@ def toadstool
 end
 
 def link 
-  $phrase = "It's Dangerous To Go Alone! Take This."
-  puts $phrase
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
 end
 
 def any_phrase
-  $any_phrase = $phrase
-  $phrase = "Do A Barrel Roll!"
-  puts $phrase
-  $phrase
+  any_phrase = phrase
+  phrase = "Do A Barrel Roll!"
+  puts phrase
 end
